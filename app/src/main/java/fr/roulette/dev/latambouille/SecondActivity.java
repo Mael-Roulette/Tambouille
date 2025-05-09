eb94e81e388176a16fb77e5bbdc48ad75f542447
Mael <maelroulette12@gmail.com>
2025-05-09 16:20:41 +0200

    Ajout page catégorie

--- app/src/main/java/fr/roulette/dev/latambouille/SecondActivity.java
+++ app/src/main/java/fr/roulette/dev/latambouille/SecondActivity.java
@@ -2,11 +2,16 @@ package fr.roulette.dev.latambouille;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 
 public class SecondActivity extends AppCompatActivity {
+  private NavController navController;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_second);
+
+
   }
 }
--- app/src/main/java/fr/roulette/dev/latambouille/ui/home/RecipesCatFragment.java
+++ app/src/main/java/fr/roulette/dev/latambouille/ui/home/RecipesCatFragment.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import fr.roulette.dev.latambouille.AppDatabase;
 import fr.roulette.dev.latambouille.R;
+import fr.roulette.dev.latambouille.ui.CategoryAdapter;
 
 public class RecipesCatFragment extends Fragment {
+  private AppDatabase database;
+
   public RecipesCatFragment() {
     // Required empty public constructor
   }
@@ -21,12 +27,11 @@ public class RecipesCatFragment extends Fragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    database = AppDatabase.getInstance(requireContext());
   }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_recipes_cat, container, false);
 
     ImageButton backButton = view.findViewById(R.id.backButton);
@@ -34,6 +39,13 @@ public class RecipesCatFragment extends Fragment {
       NavController navController = Navigation.findNavController(v);
       navController.navigate(R.id.action_recipesCatFragment_to_homeFragment);
     });
+
+    RecyclerView categoriesRecyclerView = view.findViewById(R.id.recyclerViewCategories);
+    CategoryAdapter categoryAdapter = new CategoryAdapter(database.categoryDao().getAllCategories());
+
+    GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+    categoriesRecyclerView.setLayoutManager(layoutManager);
+    categoriesRecyclerView.setAdapter(categoryAdapter);
 
     return view;
   }
